f670c254a8575535787f8f45d3d345f41a96bf1e
kwhitejr <kwhitejr.edu@gmail.com>
2015-10-31 14:50:12 -1000

    Calculator Exercise Complete

--- calculator.js
+++ calculator.js
@@ -5,61 +5,111 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
-
+function calculatorModule() {
+  var memory = 0;
+  var total = 0;
+  var calculator = {
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+  load: function(x) {
+    if (typeof x !== 'number') {
+      return "Not valid input.";
+    }
+    total = x;
+    return x;
+  },
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+  getTotal: function() {
+    return total;
+  },
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
-
+  add: function(x) {
+    total += x;
+    return total;
+  },
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+  subtract: function(x) {
+    if (typeof x !== 'number') {
+      return "Not valid input.";
+    }
+    total -= x;
+    return total;
+  },
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+  multiply: function(x) {
+    if (typeof x !== 'number') {
+      return "Not valid input.";
+    }
+    total *= x;
+    return total;
+  },
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+  divide: function(x) {
+    if (typeof x !== 'number') {
+      return "Not valid input.";
+    }
+    total /= x;
+    return total;
+  },
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+  recallMemory: function() {
+    return memory;
+  },
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+  saveMemory: function() {
+    memory = total;
+  },
 
   /**
    * Clear the value stored at `memory`
    */
+  clearMemory: function() {
+    memory = 0;
+  },
 
   /**
    * Validation
    */
+  // validation: function(x) {
+  //   if (typeof x !== 'number') {
+  //     return "Not valid input.";
+  //   }
+  // }
 
+
+
+  };
+
+  return calculator;
+}
